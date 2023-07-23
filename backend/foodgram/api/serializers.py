ef99657d8067badcb958724efc302b9ff8d39984
Evgeniy Sorokoumov <40umov.evg@gmail.com>
2023-07-23 20:31:18 +0300

    Make foodgram/api/serializers.py, change foodgram/recipes/models.py, change requirements.txt.

---
+++ backend/foodgram/api/serializers.py
@@ -0,0 +1,67 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                            ShoppingCart, Tag)
+from rest_framework import serializers
+from users.models import Follow, User
+
+
+class CustomUserSerializer(UserSerializer):
+    """Сериализатор пользователя"""
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'id', 'username', 'first_name',
+            'last_name', 'is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        return all(
+            (request is not None,
+             not request.user.is_anonymous,
+             Follow.objects.filter(
+                 user=request.user, author=obj
+             ).exists())
+        )
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    """ Сериализатор создания пользователя. """
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'username', 'first_name',
+            'last_name', 'password')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """ Сериализатор просмотра тегов. """
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'color', 'slug')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """ Сериализатор просмотра ингридиентов. """
+    name = serializers.ReadOnlyField()
+    measurement_unit = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit')
+
+
+class IngredientRecipeSerializer(serializers.ModelSerializer):
+    """ Сериализатор связи ингридиентов и рецепта. """
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name', read_only=True)
+    measurement_unit = serializers.ReadOnlyField(
+                            source='ingredient.measurement_unit',
+                            read_only=True
+    )
+
+    class Meta:
+        model = IngredientRecipe
+        fields = ('id', 'name', 'measurement_unit', 'amount')
