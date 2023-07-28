ca03d62a0309cec9adaabb051eb5161ee41d28a4
Evgeniy Sorokoumov <40umov.evg@gmail.com>
2023-07-28 11:40:48 +0300

    Change foodgram/foodgram/settings.py.

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'rest_framework',
     'colorfield',
     'api.apps.ApiConfig',
     'recipes.apps.RecipesConfig',
