4b6bf762c90a799166646d0c283472a47d251ba7
Evgeniy Sorokoumov <40umov.evg@gmail.com>
2023-07-28 09:01:32 +0300

    Change: foodgram/api/urls.py and views.py, foodgram/foodgram/urls.py, foodgram/recipes/admin.py. Make: foodgram/api/filters.py,  migrate tagrecipe.

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -20,11 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api', include('api.urls'))
+    path('api', include('api.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
